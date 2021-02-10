619e20bdb8d19f510b506ebe6d08b010a9404504
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-10 23:53:50 +0100

    Refactoring and add Dialog API from Material UI

--- src/components/ImageResult.js
+++ src/components/ImageResult.js
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { GridList, GridListTile } from "@material-ui/core";
+import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
 import { IconButton } from "@material-ui/core";
 import ZoomInIcon from "@material-ui/icons/ZoomIn";
 import { Dialog } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    color: "white",
+  },
+}));
 
 const ImageResult = ({ images }) => {
+  const [open, setOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+
+  const handleOpen = (img) => {
+    setOpen(true);
+    setCurrentImage(img);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const classes = useStyles();
   let imageListContent;
 
   if (images) {
     imageListContent = (
       <GridList cols={3}>
         {images.map((img) => (
-          <GridListTile
-            title={img.tags}
-            key={img.id}
-            subtitle={
-              <span>
-                by <strong>{img.user}</strong>
-              </span>
-            }
-            actionIcon={
-              <IconButton onClick={() => this.handleOpen(img.largeImageURL)}>
-                <ZoomInIcon color="white" />
-              </IconButton>
-            }
-          >
-            <img src={img.largeImageURL} alt="" />
+          <GridListTile key={img.id}>
+            <img src={img.largeImageURL} alt={img.tags} />
+            <GridListTileBar
+              title={img.tags}
+              subtitle={<span>by: {img.user}</span>}
+              actionIcon={
+                <IconButton
+                  aria-label={`info about ${img.title}`}
+                  className={classes.icon}
+                  onClick={() => handleOpen(img.largeImageURL)}
+                >
+                  <ZoomInIcon color="white" />
+                </IconButton>
+              }
+            />
           </GridListTile>
         ))}
       </GridList>
@@ -35,9 +54,14 @@ const ImageResult = ({ images }) => {
   } else {
     imageListContent = null;
   }
-
-  const actions = [<Button label="Close" primary={true} />];
-  return <div>{imageListContent}</div>;
+  return (
+    <div>
+      {imageListContent}
+      <Dialog modal={false} open={open} onBackdropClick={handleClose}>
+        <img src={currentImage} alt="currentImage" style={{ width: "100%" }} />
+      </Dialog>
+    </div>
+  );
 };
 
 ImageResult.propTypes = {
